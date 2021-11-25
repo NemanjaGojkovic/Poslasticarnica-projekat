a4ebff7beedb747d9fab839cf9d0d4bfbd8406ca
NemanjaGojkovic <n.gojko6@gmail.com>
2021-11-25 10:47:23 +0100

    prvi commit

---
+++ skript/jqueryScript.js
@@ -0,0 +1,115 @@
+
+
+$("*#poIzboru").click(function()
+{
+alert("Mogući sastojci: čokolada, bela čokolada, lešnik, keks, orah, vanila, karamela, nutela, kinder čokolada, višnja, pomorandža, ananas, jagoda, jabuka");
+});
+
+
+$("#kolacic").click(function(){
+    document.write("Neki tekstic");
+})
+
+
+$("#boem").click(function(){
+    window.open("Slike/torte/boem.jpg");
+})
+
+$("#dobos").click(function(){
+    window.open("Slike/torte/dobos.jpg");
+})
+
+$("#esterhazi").click(function(){
+    window.open("Slike/torte/esterhazi.jpg");
+})
+
+$("#kinder").click(function(){
+    window.open("Slike/torte/kinder.jpg");
+})
+
+$("#nugat").click(function(){
+    window.open("Slike/torte/nugat.jpg");
+})
+
+$("#snikers").click(function(){
+    window.open("Slike/torte/snikers.jpg");
+})
+
+$("#svarcvald").click(function(){
+    window.open("Slike/torte/svarcvald.jpg");
+})
+
+$("#vocniMiks").click(function(){
+    window.open("Slike/torte/vocni miks.jpg");
+})
+
+$("#baklava").click(function(){
+    window.open("Slike/kolaci/baklava.jpg");
+})
+
+$("#cizKejk").click(function(){
+    window.open("Slike/kolaci/ciz kejk.jpg");
+})
+
+$("#krempita").click(function(){
+    window.open("Slike/kolaci/krempita.jpg");
+})
+
+$("#princezKrofna").click(function(){
+    window.open("Slike/kolaci/princez krofna.jpg");
+})
+
+$("#rumKocka").click(function(){
+    window.open("Slike/kolaci/rum kocka.jpg");
+})
+
+$("#tiramisu").click(function(){
+    window.open("Slike/kolaci/tiramisu.jpg");
+})
+
+$("#tulumba").click(function(){
+    window.open("Slike/kolaci/tulumba.jpg");
+})
+
+$("#bajadera").click(function(){
+    window.open("Slike/poslastice/bajadera.jpg");
+})
+
+$("#madjarica").click(function(){
+    window.open("Slike/poslastice/madjarica.jpg");
+})
+
+$("#rafaelo").click(function(){
+    window.open("Slike/poslastice/rafaelo.jpg");
+})
+
+$("#rozen").click(function(){
+    window.open("Slike/poslastice/rozen.jpg");
+})
+
+$("#betmen").click(function(){
+    window.open("Slike/decije torte/betmen.jpg");
+})
+
+$("#garfild").click(function(){
+    window.open("Slike/decije torte/garfild.jpg");
+})
+
+$("#majnkraft").click(function(){
+    window.open("Slike/decije torte/majnkraft.jpg");
+})
+
+$("#mikiIMini").click(function(){
+    window.open("Slike/decije torte/miki i mini.jpg");
+})
+
+$("#spajdermen").click(function(){
+    window.open("Slike/decije torte/spajdermen.jpg");
+})
+
+
+
+
+
+
+
